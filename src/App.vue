2088637e59e6417dc459cf2f9efe40c03b3da0d1
kofrann <kofran.cl@gmail.com>
2022-03-08 21:55:12 -0300

    primera subida vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,86 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="display-1 my-5">
+            Esta es mi Primera Tabla de datos con Vue
+          </h1>
+          {{ getData() }}
+          <h2>{{ notebook.procesador }}</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Comuna</th>
+                <th scope="col">Id Comuna</th>
+                <th scope="col">Id Provincia</th>
+                <th scope="col">Id Region</th>
+                <th scope="col">Provincia</th>
+                <th scope="col">Región</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(trabajador, i) in trabajadores" :key="i">
+                <td>{{ i + 1 }}</td>
+                <td>{{ trabajador.comuna }}</td>
+                <td>{{ trabajador.id_comuna }}</td>
+                <td>{{ trabajador.id_provincia }}</td>
+                <td>{{ trabajador.id_region }}</td>
+                <td>{{ trabajador.provincia }}</td>
+                <td>{{ trabajador.region }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import axios from "axios";
+//let saludo = "Hola Mundo";
+//las funciones se declaran aca, pero deben declararse en una propiedad llamada method
+//vmethod atajo
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      saludo: "Hola Mundo!(Estoy dentro de Vue)",
+      gatos: 2,
+      casado: false,
+      notebook: {
+        procesador: "Amd Rysen 9 4900mhz",
+      },
+      trabajadores: [],
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const URL =
+          "https://us-central1-apis-varias-mias.cloudfunctions.net/regiones_comunas";
+        /*
+        const URL = "./data.json";
+        const request = await fetch(URL);
+        https://us-central1-apis-varias-mias.cloudfunctions.net/regiones_comunas
+        const data = await request.json();
+        this.trabajadoresEntel = data; */
+        const request = await axios.get(URL);
+        this.trabajadores = request.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+//aca importamos  el boostrap
 Vue.config.productionTip = false;
 
 new Vue({
